9b8aabb356ad9e621f1949fb8b6f869985d22db5
JisuLee <9033jisu@naver.com>
2021-09-23 13:20:47 +0900

    new text

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@
 
 //   $(".store")
 // });
+
+$(function(){
+  const write = document.getElementById("write");
+  let wList = ["환경 문제 조사", "경각심을 일으키는 보고서 발표", "직접적인 행동인 캠페인"];
+  let i=0;
+  while(i<wList.length){
+  write.innerHTML += "<div>"+wList[i]+"</div><br>"
+  i++
+  }
+});
